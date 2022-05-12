Fix setup. Improve features computation and Juarez.

uscecg/features.py
@@ -31,9 +31,12 @@ def run_emd(signal, nb_level_decomp=5, nb_comp_retain=3):
 
 # Loop for extracting time and frequency domain HRV using rpeaks information
 
-def extract_time_and_freq_features(signal, sampling_rate=512, return_peaks=False):
+def extract_time_and_freq_features(signal, sampling_rate=512, return_peaks=False, run_emd_=True):
 
-    emd_cleaned_sig = run_emd(signal)
+    if run_emd_:
+        emd_cleaned_sig = run_emd(signal)
+    else:
+        emd_cleaned_sig = signal
 
     # Extracting rpeaks using hamilton algorithm
     peaks = biosppy.signals.ecg.ecg(signal=emd_cleaned_sig, sampling_rate=sampling_rate, show=False)[2]
@@ -46,30 +49,35 @@ def extract_time_and_freq_features(signal, sampling_rate=512, return_peaks=False
         return peaks, features
     return features
 
-def extract_dwt_features(signal, threshold = 0.15):
+def run_dwt(signal, threshold = 0.15):
+
+    # Decompose into wavelet components, to the level selected:
+    coeffs = pywt.wavedec(signal, 'db4', level=6)
+    coeffs[1:] = [pywt.threshold(level_coeffs, threshold*np.max(level_coeffs)) for level_coeffs in coeffs[1:]]
+
+    return pywt.waverec(coeffs, 'db4')
+
+
+def extract_dwt_features(signal, threshold = 0.15, run_dwt_=True):
     """
     Calculate Detrended Fluctuation Analysis (DFA) and Sample Entropy after a filtering using the discrete
     wavelet transform.
     :param threshold: # Threshold for filtering, based on "https://arxiv.org/ftp/arxiv/papers/1703/1703.00075.pdf"
     :return:
     """
-    #
+    if run_dwt_:
+        signal = run_dwt(signal, threshold)
 
-    # Decompose into wavelet components, to the level selected:
-    coeffs = pywt.wavedec(signal, 'db4', level=6)
-    coeffs[1:] = [pywt.threshold(level_coeffs, threshold*np.max(level_coeffs)) for level_coeffs in coeffs[1:]]
-
-    datarec = pywt.waverec(coeffs, 'db4')
-    entropy = nolds.sampen(datarec)
-    dfa = nolds.dfa(datarec)
+    entropy = nolds.sampen(signal)
+    dfa = nolds.dfa(signal)
     return pd.DataFrame({"samp_entropy":[entropy], "dfa":[dfa]})
 
 
-def extract_all_features(signal, sampling_rate=512):
-    features_hrv = extract_time_and_freq_features(signal, sampling_rate=sampling_rate)
+def extract_all_features(signal, sampling_rate=512, run_dwt_=True, run_emd_=True):
+    features_hrv = extract_time_and_freq_features(signal, sampling_rate=sampling_rate, run_emd_=run_emd_)
     selected_features = ['HRV_MeanNN', 'HRV_CVNN', 'HRV_MedianNN', 'HRV_pNN20',
                          'HRV_HTI', 'HRV_SD1SD2', 'HRV_CSI', 'HRV_CVI']
 
-    features_dwt = extract_dwt_features(signal)
+    features_dwt = extract_dwt_features(signal, run_dwt_=run_dwt_)
 
     return pd.concat([features_hrv[selected_features], features_dwt], axis=1)

uscecg/modeling.py
@@ -6,7 +6,6 @@
 
 from types import SimpleNamespace
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 from scipy.special import expit
 from pathlib import Path
@@ -18,19 +17,23 @@ def threshold_fct(x, c=10):
 
 class QuirozJuarezModel:
 
+    nominal_parameters = {'h': 2.164,
+        'c': 1.35,
+        'b': 4,
+        'g':  7,     # Time scaling
+        'to':  0,     # Time offset
+        'a1': -0.024,
+        'a2': 0.0216,
+        'a3': -0.0012,
+        'a4': 0.12}
+
+    @property
+    def parameter_names(self):
+        return list(self.nominal_parameters.keys())
+
     def __init__(self, parameters=None):
 
-        self.parameter_names = ["h", "c", "b", "a1"
-                                "a2", "a3", "a4"]
-        self.h = 2.164
-        self.c = 1.35
-        self.b = 4
-        self.a1 = -0.024
-        self.a2 = 0.0216
-        self.a3 = -0.0012
-        self.a4 = 0.12
-
-
+        self.set_parameters(self.nominal_parameters)
         if parameters is not None:
             self.set_parameters(parameters)
 
@@ -48,10 +51,10 @@ class QuirozJuarezModel:
         x1, x2, x3, x4 = R
 
         p = self
-        dRdt = [x1 - x2 - p.c*x1*x2 - x1*x2**2,
-                p.h*x1 - 3*x2 + p.c*x1*x2 + x1*x2**2 + p.b*(x4-x2),
-                x3 - x4 - p.c*x3*x4 - x3*x4**2,
-                p.h*x3 - 3*x4 + p.c*x3*x4 + x3*x4**2 + 2*p.b*(x2-x4)]
+        dRdt = [p.g*(x1 - x2 - p.c*x1*x2 - x1*x2**2),
+                p.g*(p.h*x1 - 3*x2 + p.c*x1*x2 + x1*x2**2 + p.b*(x4-x2)),
+                p.g*(x3 - x4 - p.c*x3*x4 - x3*x4**2),
+                p.g*(p.h*x3 - 3*x4 + p.c*x3*x4 + x3*x4**2 + 2*p.b*(x2-x4))]
 
         return dRdt
 
@@ -67,9 +70,11 @@ class QuirozJuarezModel:
             R_0 = (0.1, 0.1, 0.1, 0.1)
 
         if t is None:
-            t = np.linspace(0, 10, 1001)
+            t = np.linspace(self.to, 10+self.to, 1001)
+        else:
+            t += self.to
 
-        ret_val = odeint(self._cardiac_ode, R_0, t)
+        ret_val = odeint(self._cardiac_ode, R_0, t-self.to)
 
         ret_val += noise * (np.random.random(ret_val.shape) - 0.5)
         return ret_val